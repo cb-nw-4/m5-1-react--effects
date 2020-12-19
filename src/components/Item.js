c7147c9d0d73c2cd45009c86402b72ebe7212421
ferielnabet <fnabet@ipconnex.com>
2020-12-19 15:01:58 -0500

    Finishing the UI Done

---
+++ src/components/Item.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Item = ({id, name, cost, value, numOwned, handleClick}) => {
+    return (
+
+        <Button onClick= {handleClick}>
+            <Div>
+                <h3>{name}</h3>
+                <p>Cost: {cost} cookie(s): Produces {value} cookies/second</p>
+            </Div>
+
+            <h1>
+                {numOwned}
+            </h1>
+        </Button>
+        
+        
+
+    );
+}
+
+const Button = styled.button`
+    display: flex;
+    background-color: transparent;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-bottom: 1px solid gray;
+    justify-content: space-between;
+
+    & h1{
+        margin-left: 10px;
+        align-self: center;
+    }
+`
+
+
+const Div = styled.div`
+
+    align-self: flex-start;
+
+    & h3{
+        text-align: start;
+        padding: 10px 0;
+    }
+
+    & p{
+        color: grey;
+    }
+
+
+`
+export default Item;
