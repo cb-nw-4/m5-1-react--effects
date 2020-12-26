7d8976f944b9ac52ded5f4e7e104174757fb3af5
Elodie L <67484805+EL-Bloom@users.noreply.github.com>
2020-12-26 18:47:47 -0500

    update count

--- src/components/ClickedItem.js
+++ src/components/ClickedItem.js
@@ -1,25 +1,56 @@
-// import React from "react";
+import React, {useState, useEffect} from "react";
 
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const ClickedItem = ({value, onClickFunction}) => {
-//     const newPurchasedItem = () => {
-//         onClickFunction(value)
-//     }
-//   return (
-//     <a onClick={newPurchasedItem} >
-//         {value}
-//     </a>
-//   );
-// };
+const ClickedItem = ({key, value, cookieCount, setCookieCount, purchasedItems }) => {
 
-// const Wrapper = styled.div`
-//   height: 100vh;
-//   display: grid;
-//   place-content: center;
-//   text-align: center;
-// `;
+    const { cursor, grandma, farm } = purchasedItems;
+    const [numOwned, setNumOwned] = useState(0);
+    const [categorySelected, setCategorySelected] = useState(false);
+    const [isClicked, setIsClicked] = useState([""]);
+  
+  
+  
+      const [allCategory, setAllCategory] = useState(purchasedItems);
+    const handleClick = (event) => {
+        setIsClicked(!isClicked);
+      
+    
+        if (cookieCount > 0) {
+          setCookieCount(cookieCount - event.currentTarget.value);
+          // } else {
+          //   return alert("You are out of cookies!");
+        }
+       
+          if (event.currentTarget.id === Object.keys(purchasedItems)) {
+            setCategorySelected(true);
+            setIsClicked(event.currentTarget.id);
+      
+          }
+        
+    
+      };
+  return (
+      
+    <Button onClick={handleClick} >
+        {value}
+    </Button>
+   
+  );
+};
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  place-content: center;
+  text-align: center;
+`;
+const Button = styled.button`
+ 
+
+ 
+`;
 
 
 
-// export default ClickedItem;
+export default ClickedItem;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,59 +1,47 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import ClickedItem from "./ClickedItem";
 
 const Item = ({ items, cookieCount, setCookieCount, purchasedItems }) => {
   const { cursor, grandma, farm } = purchasedItems;
   const [numOwned, setNumOwned] = useState(0);
   const [categorySelected, setCategorySelected] = useState(false);
-  const [cost, setCost] = useState(0);
-    const [ isClicked, setIsClicked] = useState([""])
+  const [isClicked, setIsClicked] = useState([""]);
+
   const [allCategory, setAllCategory] = useState(purchasedItems);
-  console.log(allCategory.cursor);
+
+  console.log(allCategory);
 
   useEffect(() => {
-if(categorySelected) {
-  setNumOwned(numOwned + 1)
-  console.log(isClicked);
-}
-    
-  }, [isClicked]);
-
-  const handleClick = (event) => {
-    console.log(event.currentTarget.id);
-    setIsClicked(!isClicked);
-    const itemsName = Object.keys(purchasedItems);
-
-    console.log(categorySelected);
-    if (cookieCount > 0) {
-      setCookieCount(cookieCount - event.currentTarget.value);
-    } else {
-      return alert("You are out of cookies!");
+    if (categorySelected) {
+      setNumOwned(numOwned + 1);
+      console.log(isClicked);
     }
-    const newItemPurchased = itemsName.map((item) => {
-      if (event.currentTarget.id === item) {
-        setCategorySelected(true);
-        setIsClicked(item);
-        console.log(isClicked);
-      }
-    });
-    return newItemPurchased;
-  };
+  }, [isClicked]);
 
   const fullList = items.map((it) => {
     return (
       <Markup>
         <List>
-          <button id={it.id} key={it.id} value={it.cost} onClick={handleClick}>
-            <Items key={it.id}>
+        
+          <ClickedItem
+            id={it.id}
+            key={it.id}
+            value={it.cost}
+            purchasedItems={purchasedItems}
+            setCookieCount ={setCookieCount}
+            cookieCount={cookieCount}
+          >
+           <Items key={it.id}>
               <Div>
                 <Title>{it.name}</Title>
                 <Description>
                   Cost: {it.cost} cookie(s). Produces {it.value} cookies/second.
                 </Description>
               </Div>
-              <Total key={it.id}>{numOwned}</Total>
+           
             </Items>
-          </button>
+            </ClickedItem>
         </List>
       </Markup>
     );
@@ -81,14 +69,6 @@ const Title = styled.h1``;
 const Description = styled.p`
   display: flex;
   flex-direction: column;
-`;
-const ClickedItem = styled.a`
-  color: white;
-`;
-const Total = styled.h2`
-  margin-top: 30px;
-  display: flex;
-  padding-left: 20px;
 `;
 
 export default Item;
