5663d8e2f39bdad92726da05e8368e60c37c5b04
Vanessa Chan <chw.vanessa@gmail.com>
2021-01-07 17:19:34 -0500

    Ex-1

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import cookieSrc from "../cookie.svg";
+import Item from "./Item";
 
 const items = [
   { id: "cursor", name: "Cursor", cost: 10, value: 1 },
@@ -34,7 +35,18 @@ const Game = () => {
 
       <ItemArea>
         <SectionTitle>Items:</SectionTitle>
-        {/* TODO: Add <Item> instances here, 1 for each item type. */}
+        {/* TODO: Add <Item> instances here, 1 for each item type. */
+        items.map((item)=>{
+          return <Item 
+          key = {item.id}
+          name={item.name} 
+          cost={item.cost} 
+          value={item.value}
+          numOwned = {purchasedItems[item.id]}
+          handleClick = {console.log("rendered")}
+          />
+        })
+        }
       </ItemArea>
       <HomeLink to="/">Return home</HomeLink>
     </Wrapper>
---
+++ src/components/Item.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+import GlobalStyles from "./GlobalStyles";
+
+const Item = ({name, cost, value, numOwned, handleClick }) =>{
+    return (
+    <ItemLine handleClick={handleClick}>
+        <div>
+        <Name>{name}</Name>
+        <p>Cost: {cost} cookie(s). Produces {value} cookies/second.</p>
+        </div>
+        <Number>{numOwned}</Number>
+    </ItemLine>
+    );
+
+};
+
+const ItemLine = styled.button`
+    display: flex;
+    align-items: center;
+    border: none;
+    border-bottom: 1px solid grey;
+    padding-bottom: 20px;
+    padding-top: 24px;
+    justify-content: space-between;
+    color: white;
+    background-color: transparent;
+
+`;
+
+const Name = styled.p`
+    font-size:22px;
+    font-weight: bold;
+    text-align: left;
+`;
+
+const Number = styled.p`
+    width: 70px;
+    text-align: center;
+    font-size: 32px;
+`;
+
+export default Item;
